a0db038753269bd79296cba6f36b3c8c9d3f834a
KVC <yulinkeji@KVC.local>
2015-04-17 15:38:37 +0800

    Initial Commit

---
+++ Hotel/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hotel
+//
+//  Created by KVC on 15-4-17.
+//  Copyright (c) 2015年 KVC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
